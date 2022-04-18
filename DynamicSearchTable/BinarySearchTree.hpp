2a34b6631e2eb78290ed277273514f7f554903dc
qiaoqiaoLF <qwerewqwer4@gmail.com>
2022-04-19 00:13:18 +0800

    Modify BinarySearchTree

--- DynamicSearchTable/BinarySearchTree.hpp
+++ DynamicSearchTable/BinarySearchTree.hpp
@@ -67,23 +67,136 @@ void BinarySearchTree<elemType, Key>::deleteTree(Node *t)
     t = nullptr;
 }
 
+// recursion
+// template <typename elemType, typename Key>
+// elemType *BinarySearchTree<elemType, Key>::find(const Key &k) const
+// {
+//     return find(k, root);
+// }
+
+// iteration
 template <typename elemType, typename Key>
 elemType *BinarySearchTree<elemType, Key>::find(const Key &k) const
 {
-    return find(k, root);
+    Node *p = root;
+    while (p && p->key != k)
+    {
+        if (p->key < k)
+            p = p->right;
+        else
+            p = p->left;
+    }
+    if (p)
+        return &(p->data);
+    return nullptr;
 }
 
+// recursion
+//  template <typename elemType, typename Key>
+//  void BinarySearchTree<elemType, Key>::insert(const elemType &x, const Key &k)
+//  {
+//      insert(x, k, root);
+//  }
+
+// iteration
 template <typename elemType, typename Key>
 void BinarySearchTree<elemType, Key>::insert(const elemType &x, const Key &k)
 {
-    insert(x, k, root);
+    if (!root)
+    {
+        root = new Node(x, k);
+        return;
+    }
+    Node *p = root;
+    while (p)
+    {
+        if (p->key > k)
+            if (p->left)
+                p = p->left;
+            else
+            {
+                p->left = new Node(x, k);
+                return;
+            }
+        else if (p->key < k)
+            if (p->right)
+                p = p->right;
+            else
+            {
+                p->right = new Node(x, k);
+                return;
+            }
+        else
+            return;
+    }
 }
 
-template <typename elemType, typename Key>
-void BinarySearchTree<elemType, Key>::remove(const Key &k)
-{
-    remove(k, root);
-}
+// recursion
+ template <typename elemType, typename Key>
+ void BinarySearchTree<elemType, Key>::remove(const Key &k)
+ {
+     remove(k, root);
+ }
+
+// iteration
+// template <typename elemType, typename Key>
+// void BinarySearchTree<elemType, Key>::remove(const Key &k)
+// {
+//     Node *p = root;
+//     Node **parent = nullptr;
+//     Node *temp = nullptr;
+//     while (p && p->key != k)
+//     {
+//         if (p->key > k)
+//         {
+//             parent = &(p->left);
+//             p = p->left;
+//         }
+//         else
+//         {
+//             parent = &(p->right);
+//             p = p->right;
+//         }
+//     }
+//     if (!p)
+//         return;
+//     else
+//     {
+//         if (!p->left && !p->right)
+//         {
+//             delete p;
+//             p = nullptr;
+//             *parent = nullptr;
+//         }
+//         else if (!p->left && p->right)
+//         {
+//             *parent = p->right;
+//             delete p;
+//             p = nullptr;
+//         }
+//         else if (p->left && !p->right)
+//         {
+//             *parent = p->left;
+//             delete p;
+//             p = nullptr;
+//         }
+//         else
+//         {
+//             temp = p;
+//             p = p->right;
+//             while (p->left)
+//             {
+//                 temp = p;
+//                 p = p->left;
+//             }
+//             (*parent)->data = p->data;
+//             (*parent)->key = p->key;
+//             temp->left = p->right;
+//             delete p;
+//             p = nullptr;
+//         }
+//     }
+// }
 
 template <typename elemType, typename Key>
 elemType *BinarySearchTree<elemType, Key>::find(const Key &k, Node *t) const
@@ -106,8 +219,10 @@ void BinarySearchTree<elemType, Key>::insert(const elemType &x, const Key &k, No
     }
     if (k > t->key)
         insert(x, k, t->right);
-    else
+    else if (k < t->key)
         insert(x, k, t->left);
+    else
+        return;
 }
 
 template <typename elemType, typename Key>
